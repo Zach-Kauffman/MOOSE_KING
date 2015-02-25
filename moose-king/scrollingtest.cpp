0e506d946114d25383a584f3f855efdcaf3d2ce8
Cade Conklin <cadecon6@gmail.com>
2015-02-25 11:23:58 -0800

    updated

--- moose-king/scrollingtest.cpp
+++ moose-king/scrollingtest.cpp
@@ -13,10 +13,10 @@ int main()
     sf::Sprite background;
     sf::Texture b;
     sf::CircleShape circle(10);
-    sf::RectangleShape platform(sf::Vector2f(30,5));
+    sf::RectangleShape platform(sf::Vector2f(50,20));
 
-    float x = 500,x1 = 0,y = 0, y1 = 0, gravity = 0,recx = 200,recy = 200;
-    bool jump = false, ground = true, down = true;
+    float x = 500,x1 = 0,y = 320, y1 = 0, gravity = 0,recx = 200,recy = 200;
+    bool jump = false, ground = true, down = true, scroll = false;
     if (!b.loadFromFile("scrollingtest.png")){
 
     }
@@ -35,20 +35,24 @@ int main()
                 window.close();
         }
         window.clear(sf::Color::Black);
-        background.setPosition(x1,y);
+        background.setPosition(x1,0);
         platform.setPosition(sf::Vector2f(recx,recy));
         window.draw(background);
-//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) and x > 200){
-//            x--;
-//        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) and x1 <=500){
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) and x > 200){
+            x--;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) and x1 == 0){
+            x--;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) and x <= 200 and x1 <= 0){
             x1++;
             recx++;
+            scroll = true;
         }
-//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) and x < 800){
-//            x++;
-//        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) and x1 >= -1500){
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) and x < 800){
+           x++;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) and x >= 800 and x1 >= -1000){
             x1--;
             recx--;
         }
@@ -58,19 +62,24 @@ int main()
             ground = false;
             down = false;
         }
-        if (jump == true){
-            y+=gravity;
+        if (jump == true and down == false){
+            y-=gravity;
             gravity-=.01;
         }
-        if (y >= 50){
+        if (y <= 270){
             down = true;
         }
-        if (y <= 5 and down == true){
-            y = 5;
+        if (jump == true and down == true){
+            y-=gravity;
+            gravity+=-.01;
+        }
+        if (y >= 320 and down == true){
+            y = 320;
+            recy = 200;
             jump = false;
             ground = true;
         }
-        circle.setPosition(x,325);
+        circle.setPosition(x,y);
         window.draw(circle);
         window.draw(platform);
         window.display();
